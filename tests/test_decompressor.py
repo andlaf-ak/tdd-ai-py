9907d305f00471bfca70d671856660344c11b78a
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-09-07 18:43:21 +0100

    Remove redundant test for length reading and streamline test for single character tree deserialization

--- tests/test_decompressor.py
+++ tests/test_decompressor.py
@@ -4,15 +4,6 @@ from tdd_ai_py.decompressor import Decompressor
 
 
 class TestDecompressor:
-    def test_reads_4_byte_length_from_input_stream(self) -> None:
-        # Length (4 bytes) + dummy tree data (2 bytes)
-        data_stream = BytesIO(b"\x00\x00\x00\x07\xB0\x80")
-        decompressor = Decompressor()
-
-        decompressor.decompress(data_stream)
-
-        assert decompressor.get_length() == 7
-
     def test_reads_serialized_tree_for_single_character(self) -> None:
         # Length (4 bytes) + serialized tree for 'a' (9 bits = 2 bytes with padding)
         # Tree: "101100001" for character 'a'
@@ -22,6 +13,10 @@ class TestDecompressor:
 
         decompressor.decompress(data_stream)
 
+        # Verify length reading
+        assert decompressor.get_length() == 1
+
+        # Verify tree deserialization
         tree = decompressor.get_tree()
         assert tree.is_leaf
         assert tree.character == "a"
