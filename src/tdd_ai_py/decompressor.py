7bb186d8b66f08c902a4e53ee6275dd4edf4538d
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-09-08 18:01:30 +0100

    Refactor HuffmanDecompressor to simplify decompress method and remove unnecessary attributes

--- src/tdd_ai_py/decompressor.py
+++ src/tdd_ai_py/decompressor.py
@@ -1,26 +1,20 @@
 import struct
 from io import BytesIO
-from typing import BinaryIO, Optional, cast
+from typing import BinaryIO, cast
 
 from .bit_reader import BitReader
 from .data_decoder import decode_data
-from .huffman_tree_builder import HuffmanNode
 from .tree_deserializer import deserialize_tree
 
 
+def read_big_endian_int(stream: BytesIO) -> int:
+    """Read a 4-byte big-endian integer from stream."""
+    return cast(int, struct.unpack(">I", stream.read(4))[0])
+
+
 class HuffmanDecompressor:
-    def __init__(self) -> None:
-        self._length: Optional[int] = None
-        self._tree: Optional[HuffmanNode] = None
-
     def decompress(self, input_stream: BytesIO, output_stream: BinaryIO) -> None:
-        self._length = self._read_big_endian_int(input_stream)
-
+        length = read_big_endian_int(input_stream)
         bit_reader = BitReader(input_stream)
-        self._tree = deserialize_tree(bit_reader)
-
-        decode_data(self._tree, bit_reader, self._length, output_stream)
-
-    def _read_big_endian_int(self, stream: BytesIO) -> int:
-        bytes_data: bytes = stream.read(4)
-        return cast(int, struct.unpack(">I", bytes_data)[0])
+        tree = deserialize_tree(bit_reader)
+        decode_data(tree, bit_reader, length, output_stream)
