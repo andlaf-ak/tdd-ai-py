0d6dabee2f170075711a5762468ee1c6d4200606
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-09-08 14:21:28 +0100

    Refactor create_frequency_map to use Counter for frequency calculation

--- src/tdd_ai_py/frequency_counter.py
+++ src/tdd_ai_py/frequency_counter.py
@@ -1,10 +1,12 @@
+from collections import Counter
 from typing import BinaryIO, Dict
 
 from .stream_utils import iter_bytes
 
 
 def create_frequency_map(input_stream: BinaryIO) -> Dict[int, int]:
-    frequency_map: Dict[int, int] = {}
-    for byte_value in iter_bytes(input_stream):
-        frequency_map[byte_value] = frequency_map.get(byte_value, 0) + 1
-    return frequency_map
+    """Create frequency map using functional programming approach.
+
+    Uses functional composition: stream -> iterable -> counter -> dict
+    """
+    return dict(Counter(iter_bytes(input_stream)))
